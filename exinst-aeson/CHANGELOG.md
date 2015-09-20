fa1b181f7abffe52c999c19a041aa7fb3cd4c8e8
Renzo Carbonara <gnuk0001@gmail.com>
2015-09-20 16:04:48 -0300

    exinst-aeson: bump dependencies on aeson and singletons

--- exinst-aeson/CHANGELOG.md
+++ exinst-aeson/CHANGELOG.md
@@ -1,3 +1,10 @@
+# Version 0.1.0.1
+
+* Relax upper bound on `singletons` dependency.
+
+* Relax upper bound on `aeson` dependency.
+
+
 # Version 0.1
 
 * Initial release.
---
+++ exinst-aeson/nix/shell.nix
@@ -0,0 +1,22 @@
+{ compiler ? "default" }:
+
+with (import <nixpkgs> {});
+with (import <nixpkgs/pkgs/development/haskell-modules/lib.nix> { inherit pkgs; });
+
+let
+
+  haskellPackages0 =
+    if compiler == "default"
+       then pkgs.haskellPackages
+       else pkgs.haskell.packages.${compiler};
+
+  hs = haskellPackages0.override {
+    overrides = self: super: {
+        exinst = self.callPackage ../../exinst/nix/default.nix {};
+      };
+    };
+
+  drv = hs.callPackage ./default.nix {};
+in if pkgs.lib.inNixShell then drv.env else drv
+
+
