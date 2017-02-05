f9bbc6960bc634a697dc8fdd3199ed66a6ea85d5
Renzo Carbonara <gnuk0001@gmail.com>
2017-02-05 12:51:45 +0100

    add constituents to release.nix

--- release.nix
+++ release.nix
@@ -1,20 +1,33 @@
 { nixpkgs ? <nixpkgs> }:
 
 let
-  pkgs = import nixpkgs {};
-  hsLib = import <nixpkgs/pkgs/development/haskell-modules/lib.nix> { inherit pkgs; };
+pkgs = import nixpkgs {};
+hsLib = import <nixpkgs/pkgs/development/haskell-modules/lib.nix> { inherit pkgs; };
 
-  hsPkgs = pkgs.haskell.packages.ghc801.override {
-    packageSetConfig = self: super: {
-      exinst = self.callPackage (import ./exinst/default.nix) {};
-      exinst-aeson = self.callPackage (import ./exinst-aeson/default.nix) {};
-      exinst-bytes = self.callPackage (import ./exinst-bytes/default.nix) {};
-      exinst-deepseq = self.callPackage (import ./exinst-deepseq/default.nix) {};
-      exinst-hashable = self.callPackage (import ./exinst-hashable/default.nix) {};
-    };
+hsPkgs = pkgs.haskell.packages.ghc801.override {
+  packageSetConfig = self: super: {
+    exinst = self.callPackage (import ./exinst/default.nix) {};
+    exinst-aeson = self.callPackage (import ./exinst-aeson/default.nix) {};
+    exinst-bytes = self.callPackage (import ./exinst-bytes/default.nix) {};
+    exinst-deepseq = self.callPackage (import ./exinst-deepseq/default.nix) {};
+    exinst-hashable = self.callPackage (import ./exinst-hashable/default.nix) {};
   };
+};
+
+constituents = pkgs.releaseTools.aggregate {
+  name = "exinst-constituents";
+  meta.description = "Release-critical builds";
+  constituents = [
+    hsPkgs.exinst
+    hsPkgs.exinst-aeson
+    hsPkgs.exinst-bytes
+    hsPkgs.exinst-deepseq
+    hsPkgs.exinst-hashable
+  ];
+};
 
 in {
+  inherit constituents;
   inherit (hsPkgs)
     exinst
     exinst-aeson
