78301c07bf9017a50c8b7361f8144fc3cf38f50b
Renzo Carbonara <gnuk0001@gmail.com>
2017-02-05 13:15:04 +0100

    release.nix more reproducible

--- release.nix
+++ release.nix
@@ -1,34 +1,41 @@
-{ nixpkgs ? <nixpkgs> }:
+{ nixpkgsBootstrap ? <nixpkgs>
+, nixpkgs ? (import nixpkgsBootstrap {}).fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "4db7ca85474e985b4b35093abab5e34a4cb64274"; # release-16.09
+    sha256 = "1dbcclwkzphhgm6vzng4cph0haa7qbcdxdj3msdfddhv2ni2fnaw"; }
+}:
 
 let
 pkgs = import nixpkgs {};
-hsLib = import <nixpkgs/pkgs/development/haskell-modules/lib.nix> { inherit pkgs; };
 
-hsPkgs = pkgs.haskell.packages.ghc801.override {
-  packageSetConfig = self: super: {
-    exinst = self.callPackage (import ./exinst/default.nix) {};
-    exinst-aeson = self.callPackage (import ./exinst-aeson/default.nix) {};
-    exinst-bytes = self.callPackage (import ./exinst-bytes/default.nix) {};
-    exinst-deepseq = self.callPackage (import ./exinst-deepseq/default.nix) {};
-    exinst-hashable = self.callPackage (import ./exinst-hashable/default.nix) {};
-  };
+hsPackageSetConfig = self: super: {
+  exinst = self.callPackage (import ./exinst/default.nix) {};
+  exinst-aeson = self.callPackage (import ./exinst-aeson/default.nix) {};
+  exinst-bytes = self.callPackage (import ./exinst-bytes/default.nix) {};
+  exinst-deepseq = self.callPackage (import ./exinst-deepseq/default.nix) {};
+  exinst-hashable = self.callPackage (import ./exinst-hashable/default.nix) {};
+};
+
+ghc802 = pkgs.haskell.packages.ghc801.override {
+  packageSetConfig = hsPackageSetConfig;
 };
 
 constituents = pkgs.releaseTools.aggregate {
   name = "exinst-constituents";
   meta.description = "Release-critical builds";
   constituents = [
-    hsPkgs.exinst
-    hsPkgs.exinst-aeson
-    hsPkgs.exinst-bytes
-    hsPkgs.exinst-deepseq
-    hsPkgs.exinst-hashable
+    ghc802.exinst
+    ghc802.exinst-aeson
+    ghc802.exinst-bytes
+    ghc802.exinst-deepseq
+    ghc802.exinst-hashable
   ];
 };
 
 in {
   inherit constituents;
-  inherit (hsPkgs)
+  inherit (ghc802)
     exinst
     exinst-aeson
     exinst-bytes
