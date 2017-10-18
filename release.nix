e6b00770f40cfbf97310f20a02e772eaea55910a
Renzo Carbonara <gnuk0001@gmail.com>
2017-10-18 15:28:37 +0200

    release.nix: track latest nixos-17.09 channel by default

--- release.nix
+++ release.nix
@@ -1,9 +1,5 @@
-{ nixpkgsBootstrap ? <nixpkgs>
-, nixpkgs ? (import nixpkgsBootstrap {}).fetchFromGitHub {
-    owner = "NixOS";
-    repo = "nixpkgs";
-    rev = "2231575a7ddd768fbce7d8f0c75926e5f1ee5c95"; # release-17.09
-    sha256 = "0w55wvv55j88b3hzhjsi19g8n7vl4mng701qa38g1p3gjqsqqjsv"; }
+{ nixpkgs ? builtins.fetchTarball
+    "https://github.com/NixOS/nixpkgs-channels/archive/nixos-17.09.tar.gz"
 }:
 
 let
